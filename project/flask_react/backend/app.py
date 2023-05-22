97dbfd9461b1b497b1dfe604d85e20d23114e7f8
CryonicStrider <cryonicstrider+github@gmail.com>
2023-05-22 11:15:49 +0530

    Initial commit

---
+++ project/flask_react/backend/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request
+import mysql.connector
+import json
+import configparser
+
+configfile = r'./config.txt'
+
+configParser = configparser.RawConfigParser()
+configParser.read(configfile)
+conecsection = 'connector config'
+chost = configParser.get(conecsection, 'host')
+cuser = configParser.get(conecsection, 'user')
+cpasswd = configParser.get(conecsection, 'password')
+cdatabase = configParser.get(conecsection, 'database')
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello, Docker!'
+
+@app.route('/widgets')
+def get_widgets():
+	mydb = mysql.connector.connect(host=chost, user=cuser,password=cpasswd, database=cdatabase)
+	cursor = mydb.cursor()
+	cursor.execute("SELECT * FROM widgets;")
+
+	row_headers =['name', 'description']
+	
+	results = cursor.fetchall()
+	json_data = []
+	for result in results:
+		json_data.append(dict(zip(row_headers,result)))
+	return json.dumps(json_data)
+
+@app.route('/initdb')
+def db_init():
+	mydb = mysql.connector.connect(host=chost, user=cuser,password=cpasswd)
+	cursor = mydb.cursor()
+
+	cursor.execute("CREATE DATABASE IF NOT EXISTS pytcheck;")
+	mydb= mysql.connector.connect(host=chost, user=cuser,password=cpasswd, database=cdatabase)
+	cursor = mydb.cursor()
+	cursor.execute("CREATE TABLE IF NOT EXISTS widgets(name VARCHAR (255), description VARCHAR(255));")
+	mydb.commit()
+	return 'init database'
+@app.route('/cleardb')
+def db_clear():
+        db = mysql.connector.connect(host=chost, user=cuser,password=cpasswd, database = cdatabase)
+        cursor = db.cursor()
+
+        cursor.execute("DROP TABLE widgets;")
+        cursor.execute("CREATE TABLE widgets(name VARCHAR (255), description VARCHAR(255));")
+        db.commit()
+        return 'database cleared'
+
+@app.route('/addvalues')
+def add_values():
+	db = mysql.connector.connect(host=chost, user=cuser,password=cpasswd, database = cdatabase)
+	cursor = db.cursor()
+	#http post query
+	cursor.execute("SELECT * from widgets;")
+	vals = cursor.fetchall()
+	value = len(vals)
+
+	cursor.execute("INSERT INTO widgets VALUES('{}', '{}');".format("Value{}".format(value),"This is the value at table row number {}".format(value)))
+	db.commit()
+	return 'inserted value number {}'.format(value)
+
+@app.route('/acceptData', methods = ['POST'])
+def get_details():
+	_name = request.get.data('name')
+	_descript = request.get.data('description')
+	con = mysql.connector.connect(host=chost,user=cuser,password=cpasswd,database = cdatabase)
+	cursor = con.cursor()
+	cursor.execute("INSERT INTO widgets VALUES('{}','{}');".format(_name,_descript))
+	con.commit()
+	return 'inserted'
+
+
+if __name__ == "__main__":
+	app.run(host = '0.0.0.0', port = 8000)
+
---
+++ project/flask_react/src/App.js
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import axios, { formToJSON } from "axios";
+import './App.css';
+
+function App() {
+  const mapper = [{"name":"","description":""}];
+  const [jsonData, setjsonData] = useState(mapper)
+  const [nameTemp, setnameTemp] = useState('')
+  const [descTemp, setdescTemp] = useState('')
+
+  function getData(){
+	axios({
+		method: "GET",
+		url: "/widgets"
+	})
+	.then(res => {
+		setjsonData(res.data)
+	}).catch((error) => {
+		if(error.response){
+			console.log(error.response)
+			console.log(error.response.status)
+			console.log(error.response.headers)
+		}
+})}
+
+  function addData(){
+	axios({
+		method: "GET",
+		url: "/addvalues"
+	})
+  }
+  function clearDB(){
+	axios({
+		method: "GET",
+		url: "/cleardb"
+	})
+  }
+  function sendData(){
+	const val = {'name': nameTemp, 'description': descTemp}
+	console.log(val)
+	axios({
+		method: "POST",
+		url: "/acceptData",
+		data: val
+	})
+	
+  }
+  function handleSubmit(){
+	sendData()
+  }
+  function handleNameChange(event){
+	setnameTemp(event.target.value);
+  }
+  function handleDescChange(event){
+	setdescTemp(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+	<p>To get your database details: </p>
+	<button onClick={getData}>Click me!</button>
+	<table>
+		<th>Name</th><th>Description</th>
+		{jsonData.map((item) => (
+			<tr key ={item.name}>
+				{Object.values(item).map((val) => (
+					<td>&nbsp;&nbsp;{val}&nbsp;&nbsp;</td>
+				))}
+			</tr>
+		))
+		}	
+	</table>
+
+	<p> To add more values to the details: </p>
+	<button onClick={addData}>Add Data</button>
+    <p>To clear the values from the database: </p>
+	<button onClick={clearDB}>Clear DB</button>
+	<p>To add Custom Values to the database: </p>
+
+	<form name="getinfo" method='POST'>
+	<label>Name:
+	<textarea
+        type="text"
+        id="name"
+        name="name"
+		cols="25"
+        onChange={handleNameChange}
+
+    /><br/></label>
+	<label>Description:	
+	<textarea
+		name = 'desc'
+		rows="2"
+		cols="60"
+		onChange={handleDescChange}
+	/><br/></label>
+	</form>
+	<button onClick={handleSubmit}>Submit</button>
+	</header>
+    </div>
+  );
+}
+
+export default App;
